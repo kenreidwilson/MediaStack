1a8e8e30408fe944bcf40bdd3c90ceb72a31b1a4
kenreidwilson <kenreidwilson@gamil.com>
2021-03-12 20:14:26 -0500

    Added UnitOfWork Tests

---
+++ MediaStack Library Tests/Data Access Layer/UnitOfWork Tests/RepositoriesTests.cs
@@ -0,0 +1,56 @@
+﻿using MediaStack_Library.Data_Access_Layer;
+using MediaStack_Library.Model;
+using MediaStack_Testing_Library.Mocks;
+using Xunit;
+
+namespace MediaStack_Library_Tests.Data_Access_Layer.UnitOfWork_Tests
+{
+    public class RepositoriesTests
+    {
+        #region Data members
+
+        protected readonly IUnitOfWork unitOfWork;
+
+        #endregion
+
+        #region Constructors
+
+        public RepositoriesTests()
+        {
+            this.unitOfWork = new UnitOfWork<MockMediaStackContext>();
+        }
+
+        #endregion
+
+        [Fact]
+        public void Terms_GetMedia_ReturnsIRepositoryOfMedia()
+        {
+            Assert.IsAssignableFrom<IRepository<Media>>(unitOfWork.Media);
+        }
+
+        [Fact]
+        public void Terms_GetAlbums_ReturnsIRepositoryOfAlbum()
+        {
+            Assert.IsAssignableFrom<IRepository<Album>>(unitOfWork.Albums);
+        }
+
+        [Fact]
+        public void Terms_GetArtists_ReturnsIRepositoryOfArtist()
+        {
+            Assert.IsAssignableFrom<IRepository<Artist>>(unitOfWork.Artists);
+        }
+
+        [Fact]
+        public void Terms_GetCategories_ReturnsIRepositoryOfCategory()
+        {
+            Assert.IsAssignableFrom<IRepository<Category>>(unitOfWork.Categories);
+        }
+
+        [Fact]
+        public void Terms_GetTags_ReturnsIRepositoryOfTag()
+        {
+            Assert.IsAssignableFrom<IRepository<Tag>>(unitOfWork.Tags);
+        }
+
+    }
+}
